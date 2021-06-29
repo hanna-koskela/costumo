95011d33982cd6bcf503f3aa76fb7e4b57dd2f49
Hanna Koskela <haleko@yahoo.com>
2021-06-29 21:39:24 +1000

    Changed the root page to home as per discussion

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,8 +1,10 @@
 class CostumesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: :home
+  def home
+    @costumes = Costume.all
+  end
 
   def index
-    @costumes = Costume.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # get 'costumes/index'
   # get 'costumes/show'
   devise_for :users
-  root to: 'costumes#index'
+  root to: 'costumes#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :costumes
