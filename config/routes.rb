87179782f12037ba7275299668c3a41050b349e2
kjam0 <south.kay@gmail.com>
2021-06-29 20:42:38 +1000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/show'
-  get 'bookings/update'
-  get 'bookings/edit'
-  get 'bookings/destroy'
-  get 'costumes/index'
-  get 'costumes/show'
+  # get 'bookings/new'
+  # get 'bookings/create'
+  # get 'bookings/show'
+  # get 'bookings/update'
+  # get 'bookings/edit'
+  # get 'bookings/destroy'
+  # get 'costumes/index'
+  # get 'costumes/show'
   devise_for :users
   root to: 'costumes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :costumes
+  resources :bookings
 end
