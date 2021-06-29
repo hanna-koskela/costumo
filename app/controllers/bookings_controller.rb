ae1054abd0d2273a9856d2ccab2f1bbbae533d1e
Hanna Koskela <haleko@yahoo.com>
2021-06-29 19:46:41 +1000

    Setup changes

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,19 @@
+class BookingsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/show'
+  get 'bookings/update'
+  get 'bookings/edit'
+  get 'bookings/destroy'
   devise_for :users
   root to: 'costumes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
