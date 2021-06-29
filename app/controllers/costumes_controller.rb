28412818e6aa185b51948d150abf08bb15ed9443
Hanna Koskela <haleko@yahoo.com>
2021-06-29 21:20:56 +1000

    Made root page visible for visitors. Added an initial loop for costumes for index page.

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,5 +1,8 @@
 class CostumesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
+    @costumes = Costume.all
   end
 
   def show
--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,4 +1,5 @@
 class Costume < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many_attached :photos
 end
