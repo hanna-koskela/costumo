d67888c8fb40633a3550993018826aee7a7f8475
Leeten Chin <lchin@imsystems.com.au>
2021-07-01 19:25:11 +1000

    Put the latest post costumo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bookings
-  has_many :costumes, through: :bookings
+  has_many :bookings, dependent: :destroy
+  has_many :costumes, dependent: :destroy
 end
