f870c346aad1a7fceabd1e75bfce22a221121622
Talina Bay <talinabayeleva@gmail.com>
2021-07-01 20:40:16 +1000

    nested bookings so that we could create a booking through costumes#show

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   root to: 'costumes#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :costumes
+  resources :costumes do
+    resources :bookings, only: [ :create ]
+  end
   resources :bookings
 end
