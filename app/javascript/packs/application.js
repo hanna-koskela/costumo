56fe6936544ffce346ecb3389284adc814115a43
Talina Bay <talinabayeleva@gmail.com>
2021-07-01 20:06:17 +1000

    imported flatpickr to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import flatpickr from "flatpickr";
 
 Rails.start()
 Turbolinks.start()
