e8101a817e26ba621956a716cda62887667d89cd
Talina Bay <talinabayeleva@gmail.com>
2021-07-01 21:28:21 +1000

    added init_flatpickr js pack

---
+++ app/javascript/packs/init_flatpickr.js
@@ -0,0 +1,33 @@
+function initFlatpickr()
+{
+  // First we define two variables that are going to grab our inputs field. You can check the ids of the inputs with the Chrome inspector.
+  const startDateInput = document.getElementById('booking_date_start');
+  const endDateInput = document.getElementById('booking_date_end');
+
+  // Check that the query selector id matches the one you put around your form.
+  if (startDateInput) {
+  const unavailableDates = JSON.parse(document.querySelector('#costume-booking-dates').dataset.unavailable)
+  endDateInput.disabled = true
+
+  flatpickr(startDateInput, {
+    minDate: "today",
+    disable: unavailableDates,
+    dateFormat: "Y-m-d",
+  });
+
+  console.log('im in the file')
+
+  startDateInput.addEventListener("change", (e) => {
+    if (startDateInput != "") {
+      endDateInput.disabled = false
+    }
+    flatpickr(endDateInput, {
+      minDate: e.target.value,
+      disable: unavailableDates,
+      dateFormat: "Y-m-d"
+      });
+    })
+  };
+}
+
+export { initFlatpickr };
