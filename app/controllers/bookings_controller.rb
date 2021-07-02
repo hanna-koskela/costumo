b4ba49025b8ca85177dd8bba5a2ec50d5aaa682d
Hanna Koskela <haleko@yahoo.com>
2021-07-02 15:39:50 +1000

    Bookings updates

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,18 +7,33 @@ class BookingsController < ApplicationController
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+
+    redirect_to booking_path(@booking)
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
   end
 
   def index
     @bookings = Booking.where(user: current_user).where('date_end > ?', Date.today)
   end
+
+  private
+
+  def booking_params
+    params.require(:article).permit(:date_start, :date_end, :costume_id)
+  end
+
 end
