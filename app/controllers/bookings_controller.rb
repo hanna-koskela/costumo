224dbe7f6c78250fecd7308d766b6f783990d113
kjam0 <south.kay@gmail.com>
2021-07-02 22:08:11 +1000

    added booking functionality

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,27 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+  
   def new
+    @costume = Costume.find(params[:costume_id])
     @booking = Booking.new
+    @costume.costume_id = @costume
   end
 
   def create
+    @costume = Costume.find(params[:costume_id])
+    @booking = Booking.new(booking_params)
+    @booking.user_id = current_user.id
+    @booking.costume_id = params[:costume_id]
+    @booking.save
+    if @booking.save
+      puts "saved"
+      redirect_to bookings_path(@booking)
+    else
+      puts "not saved"
+      render :new
+    end
   end
 
   def show
@@ -17,4 +35,11 @@ class BookingsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date_start, :date_end, :user_id, :costume_id)
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :costumes do
-    resources :bookings, only: [ :create ]
+    resources :bookings, only: [ :create, :new, :show ]
   end
-  resources :bookings
+  resources :bookings, only: [:index]
 end
