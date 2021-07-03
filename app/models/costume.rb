1502bc683e5b7754b5c44679fc3cc2390fe3c1f6
Talina Bay <talinabayeleva@gmail.com>
2021-07-03 14:26:20 +1000

    added unavailable_dates_edit method in costume.rb for bookings#edit

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -9,4 +9,10 @@ class Costume < ApplicationRecord
       { from: range[0], to: range[1] }
     end
   end
+
+  def unavailable_dates_edit(current_booking)
+    bookings.where.not(id: current_booking.id).pluck(:date_start, :date_end).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
