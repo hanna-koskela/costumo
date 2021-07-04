6b77c36f40dae945473dfb2d69f1a4964f96fb0f
Leeten Chin <lchin@imsystems.com.au>
2021-07-04 19:46:42 +1000

    fix navbar and calendar and button style

--- app/javascript/packs/init_flatpickr.js
+++ app/javascript/packs/init_flatpickr.js
@@ -6,26 +6,30 @@ function initFlatpickr()
 
   // Check that the query selector id matches the one you put around your form.
   if (startDateInput) {
-  const unavailableDates = JSON.parse(document.querySelector('#costume-booking-dates').dataset.unavailable)
-  endDateInput.disabled = true
+    const unavailableDates = JSON.parse(document.querySelector('#costume-booking-dates').dataset.unavailable)
 
-  const f = flatpickr(startDateInput, {
-    minDate: "today",
-    disable: unavailableDates,
-    dateFormat: "d-m-Y",
-  });
+    const f = flatpickr(startDateInput, {
+      minDate: "today",
+      disable: unavailableDates,
+      dateFormat: "d-m-Y",
+    });
 
-  console.log('im in the file')
-
-  startDateInput.addEventListener("change", () => {
-    if (startDateInput != "") {
-      endDateInput.disabled = false
-    }
-    flatpickr(endDateInput, {
-      minDate: f.selectedDates[0],
+    const e = flatpickr(endDateInput, {
+      minDate: "today",
       disable: unavailableDates,
       dateFormat: "d-m-Y"
-      });
+    });
+
+    endDateInput.disabled = (endDateInput.value == "")
+
+    startDateInput.addEventListener("change", (event) => {
+      if (startDateInput.value != "") {
+        endDateInput.disabled = false
+      }
+      // set the end date default date if it is blank
+      if (endDateInput.value == "" || e.selectedDates[0] < f.selectedDates[0])  e.setDate(f.selectedDates[0]);
+      // set minimum calendar selection based on start date selection
+      e.set("minDate", f.selectedDates[0]);
     })
   };
 }
